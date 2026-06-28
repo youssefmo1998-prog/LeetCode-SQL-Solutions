4e5b55cdba68acd943a11101b50e7766fdac2b5f
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-06-28 14:10:35 -0700

    Update 185_department_top_three_salaries.sql

--- Hard/185_department_top_three_salaries.sql
+++ Hard/185_department_top_three_salaries.sql
@@ -5,7 +5,8 @@ each of the company's departments. A high earner in a department is an employee
 who has a salary in the top three unique salaries for that department.
 
 Write a solution to find the employees who are high earners in each of the departments.
-=============================================================================
+
+URL : https://leetcode.com/problems/department-top-three-salaries/description/
   */
   
 with top_three as(
