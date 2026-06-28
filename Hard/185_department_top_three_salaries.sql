8e9916b7e2946ad7ec8ad87aa96d50ebe4883956
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-06-28 09:38:35 -0700

    Update 185_department_top_three_salaries.sql

--- Hard/185_department_top_three_salaries.sql
+++ Hard/185_department_top_three_salaries.sql
@@ -1,3 +1,13 @@
+/*
+  Description:
+A company's executives are interested in seeing who earns the most money in 
+each of the company's departments. A high earner in a department is an employee 
+who has a salary in the top three unique salaries for that department.
+
+Write a solution to find the employees who are high earners in each of the departments.
+=============================================================================
+  */
+  
 with top_three as(
 select d.name as Department,e.name as Employee,e.Salary,
 dense_rank() over (partition by departmentId order by salary DESC) as ranking
