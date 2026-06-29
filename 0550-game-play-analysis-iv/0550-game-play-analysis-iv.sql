44a05bd6e715dae5804981384121c55403166c83
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-06-29 14:04:48 -0700

    Update 0550-game-play-analysis-iv.sql

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,5 +1,4 @@
 WITH FirstLogin AS (
-    -- بنجيب أول يوم تسجيل لكل لاعب
     SELECT player_id, MIN(event_date) AS first_date
     FROM Activity
     GROUP BY player_id
@@ -8,7 +7,6 @@ SELECT
     ROUND(
      COUNT(DISTINCT f.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
 FROM FirstLogin f
--- بنعمل Join مع الجدول الأصلي عشان نشوف هل نفس اللاعب عنده سجل في اليوم التالي لـ first_date
 JOIN Activity a 
   ON f.player_id = a.player_id 
   AND a.event_date = DATE_ADD(f.first_date, INTERVAL 1 DAY);
