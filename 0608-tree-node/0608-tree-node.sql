7c9d73939d0ac8d0163dd1c25fff294fb0bf57f8
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-07-03 15:21:46 +0300

    Time: 490 ms (62.5%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,13 +1,7 @@
-with tree_types as(
-select id, take,give,
-(case when take is null then 'Root'
-when take >=1 and give >=1 then 'Inner'
-when take >=1 and give is null then 'Leaf' end) as type
-from(
-select t1.id as id,t1.p_id as take,t2.id as give
-from Tree t1
-left join tree t2
-on t1.id = t2.p_id) ff
-)
-select distinct id,type
-from tree_types
+SELECT id,
+    CASE 
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM Tree;
