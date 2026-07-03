f491f6f8c2daa2ffacc9ba1b94bc06442cbf8f0f
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-07-03 15:22:53 +0300

    Update 0185-department-top-three-salaries.sql

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,11 +1,7 @@
-# Write your MySQL query statement below
-with top_three as(
-select d.name as Department,e.name as Employee,e.Salary,
-dense_rank() over (partition by departmentId order by salary DESC) as ranking
-from employee e
-left join Department d
-on e.departmentId = d.id
-)
-select Department,Employee,Salary
-from top_three
-where ranking <=3
+SELECT id,
+    CASE 
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Inner'
+        ELSE 'Leaf'
+    END AS type
+FROM Tree;
