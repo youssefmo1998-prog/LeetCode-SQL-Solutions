4d427b8616f3453a54614f4d62909af95e43d0d6
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-07-08 23:32:45 +0300

    Time: 692 ms (80.7%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,7 +1,42 @@
-select m.product_id, first_year, s.quantity,s.price
+/*select m.product_id, first_year, s.quantity,s.price
 from Sales s
 join(
 select product_id,min(year) as first_year
 from sales
 group by product_id ) M
-on s.product_id = m.product_id and s.year = m.first_year
+on s.product_id = m.product_id and s.year = m.first_year*/
+with min_year as(
+select * , DENSE_RANK() over (partition by product_id order by year) rank_year
+from sales)
+select product_id, year first_year, quantity,price
+from min_year
+where rank_year=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
