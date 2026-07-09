2dbd4d36d7c4f0d03e63622c98ab8eda4e07fddb
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-07-09 23:47:40 +0300

    Time: 1242 ms (68.41%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,7 +1,12 @@
-select user_id as buyer_id,join_date,count(o.order_id) orders_in_2019 
-from Users u
-left join (select order_id, buyer_id,item_id from orders where order_date between '2019-01-01'and '2019-12-31') o
-on u.user_id = o.buyer_id
-left join Items i
-on o.item_id = i.item_id
-group by user_id,join_date
+SELECT 
+    u.user_id AS buyer_id, 
+    u.join_date, 
+    COUNT(o.order_id) AS orders_in_2019
+FROM 
+    Users u
+LEFT JOIN 
+    Orders o ON u.user_id = o.buyer_id 
+    AND o.order_date BETWEEN '2019-01-01' AND '2019-12-31'
+GROUP BY 
+    u.user_id, 
+    u.join_date;
