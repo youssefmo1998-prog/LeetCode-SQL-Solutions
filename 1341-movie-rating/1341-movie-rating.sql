c9d304416cfcd6f996a203ec159f7a60a4477e17
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-08-27 01:50:49 +0300

    Time: 1346 ms (74.84%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,18 +1,19 @@
 (
-    SELECT u.name AS results
-    FROM MovieRating mr
-    JOIN Users u ON mr.user_id = u.user_id
-    GROUP BY u.user_id, u.name
-    ORDER BY COUNT(mr.movie_id) DESC, u.name ASC
-    LIMIT 1
+select u.name as results
+from MovieRating mr
+left join users u
+on mr.user_id = u.user_id
+group by u.name
+order by count(rating) desc,u.name
+limit 1
 )
-UNION ALL
+union all
 (
-    SELECT m.title AS results
-    FROM MovieRating mr
-    JOIN Movies m ON mr.movie_id = m.movie_id
-    WHERE mr.created_at >= '2020-02-01' AND mr.created_at <= '2020-02-29'
-    GROUP BY m.movie_id, m.title
-    ORDER BY AVG(mr.rating) DESC, m.title ASC
-    LIMIT 1
-);
+SELECT v.title
+FROM MovieRating mr
+JOIN Movies v ON mr.movie_id = v.movie_id
+WHERE mr.created_at >= '2020-02-01' AND mr.created_at <= '2020-02-29'
+GROUP BY v.title
+ORDER BY avg(rating) DESC, v.title ASC
+LIMIT 1
+)
