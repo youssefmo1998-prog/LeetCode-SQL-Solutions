8e0a2acbf2b82e8266ae092c017ca3d0fd234797
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-08-28 01:13:59 +0300

    Time: 295 ms (75.3%), Space: 0B (100%) - LeetHub

---
+++ 3220-odd-and-even-transactions/3220-odd-and-even-transactions.sql
@@ -0,0 +1,5 @@
+select transaction_date,sum(case when amount %2!=0 then amount else 0 end) odd_sum,
+sum(case when amount %2=0 then amount else 0 end) even_sum
+from transactions 
+group by transaction_date
+order by transaction_date
