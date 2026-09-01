1cb074060d44e091359420591cbfc6917e1f6b84
youssefmo1998-prog <youssef.almallah.2017@gmail.com>
2026-09-02 00:26:56 +0300

    Time: 344 ms (64.2%), Space: 0B (100%) - LeetHub

--- 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -2,6 +2,6 @@ SELECT user_id,round(avg(case when activity_type = 'free_trial' then activity_du
 round(avg(case when activity_type = 'paid' then activity_duration end),2) paid_avg_duration 
 from UserActivity
 group by user_id
-having  avg(case when activity_type = 'paid' then activity_duration end) is not null and
-avg(case when activity_type = 'free_trial' then activity_duration end) is not null
+having  paid_avg_duration is not null and
+trial_avg_duration is not null
 order by user_id
